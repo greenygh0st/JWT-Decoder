99f9c736375a6eb7347b1324757845ced505b061
Dale Myszewski II <dale.myszewski@seagate.com>
2019-03-12 03:55:26 -0500

    Add Xamarin version because of missing Mono libs

---
+++ JWTDecoder.Xamarin/Helpers/DateTimeHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace JWTDecoder.Xamarin.Helpers
+{
+    internal static class DateTimeHelpers
+    {
+        internal static DateTime FromUnixTime(long unixTime)
+        {
+            return epoch.AddSeconds(unixTime);
+        }
+        internal static readonly DateTime epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    }
+}
